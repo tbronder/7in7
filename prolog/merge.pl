67c9b7f3f5c61483673a6ff3990ddb3195b0594e
Tyler Bronder <tbronder@alum.mit.edu>
2011-12-14 17:52:04 -0500

    Some basic merge and reverse prolog implementations.

--- prolog/merge.pl
+++ prolog/merge.pl
@@ -1,13 +1,23 @@
+/*
+ * split_list doesn't techinically split a list, rather dumps alternating elements into the two
+ * outputs. I.e. [1,2,3,4] -> [1,3] and [2,4]
+ */
 split_list([], [], []).
 split_list([Head|[]], [Head], []).
 split_list([Head1|[Head2|Tail]], X, Y) :- split_list(Tail, A, B), append([Head1], A, X), append([Head2], B, Y).
 
+/*
+ * merges two lists together, smallest to largest
+ */
 merge([], [], []).
 merge(A, [], A).
 merge([], B, B).
 merge([HeadA|TailA], [HeadB|TailB], [HeadA|X]) :- HeadA =< HeadB, merge(TailA, [HeadB|TailB], X).
 merge([HeadA|TailA], [HeadB|TailB], [HeadB|X]) :- HeadA > HeadB, merge([HeadA|TailA], TailB, X).
 
+/*
+ * Combine to make mergesort!
+ */
 mergesort([], []).
 mergesort([A|[]], [A]).
 mergesort([Head|Tail], S) :- split_list([Head|Tail], Left, Right), mergesort(Left, LeftSort), mergesort(Right, RightSort), merge(LeftSort, RightSort, S).
--- prolog/reverse.pl
+++ prolog/reverse.pl
@@ -1,7 +1,13 @@
+/*
+ * List reversal implementation using append.
+ */
 rev([], []).
 rev([Head|[]], [Head]).
 rev([Head|Tail], A) :- append(B, [Head], A), rev(Tail, B).
 
+/*
+ * List reversal with an intermediate accumulator.
+ */
 rev2(StartList, Solution) :- rev2(StartList, [], Solution).
 rev2([], Accum, Accum).
 rev2([Head|Tail], Accum, Solution) :- rev2(Tail, [Head|Accum], Solution).
